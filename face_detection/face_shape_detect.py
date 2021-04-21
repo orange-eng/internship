689164628dbc397db796f5c53ebb47fac5a203d4
v_chengzcao(曹成志) <v_chengzcao@tencent.com>
2021-04-21 11:13:21 +0800

    Detect person's mouth and draw a rectangle

--- face_detection/face_shape_detect.py
+++ face_detection/face_shape_detect.py
@@ -8,7 +8,7 @@ path = os.path.abspath(os.path.dirname(sys.argv[0]))
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(path + "/face_shape/shape_predictor_68_face_landmarks.dat")
 # cv2读取图像
-img = cv2.imread("img/picture1.jpg")
+img = cv2.imread(path + "/img/picture6.jpg")
 # 取灰度
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 # 人脸数rects
@@ -22,7 +22,7 @@ for i in range(len(rects)):
         cv2.circle(img, pos, 2, color=(0, 255, 0))
         # 利用cv2.putText输出1-68
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, str(idx + 1), None, font, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+        cv2.putText(img, str(idx + 1), pos, font, 0.4, (0, 0, 255), 1, cv2.LINE_AA)
 cv2.namedWindow("img", 2)
 cv2.imshow("img", img)
 cv2.waitKey(0)
