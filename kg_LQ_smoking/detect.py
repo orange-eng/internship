da7e25536c9ea1e561d4647021a6e3ccfa2e03ce
orange-eng <972353371@qq.com>
2021-05-28 15:49:56 +0800

    some tiny changes

--- kg_LQ_smoking/detect.py
+++ kg_LQ_smoking/detect.py
@@ -31,8 +31,8 @@ def detect(save_img=False,output_file=None,source_file=None):
     
     out = output_file
     source = source_file
-    # print('out=',out)
-    # print("source=",source)
+    print('out=',out)
+    print("source=",source)
 
     webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
 
@@ -104,13 +104,14 @@ def detect(save_img=False,output_file=None,source_file=None):
         # Process detections
         for i, det in enumerate(pred):  # detections per image
             image_total = image_total + 1
-            if image_total % 5 == 0:
+            if image_total % 1 == 0:
                 if webcam:  # 电脑自带相机,batch_size >= 1
                     p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
                 else:
                     p, s, im0 = path, '', im0s
 
                 save_path = str(Path(out) / Path(p).name)
+                print("save_path=",save_path)
                 txt_path = str(Path(out) / Path(p).stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
                 s += '%gx%g ' % img.shape[2:]  # print string
                 gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
@@ -138,68 +139,68 @@ def detect(save_img=False,output_file=None,source_file=None):
                             # 嘴唇与烟头的交集区域判断逻辑
                             ############################
 
-                            # rects = detector(im0,1)
-                            # for (i, rect) in enumerate(rects):
-                            #     # 确定面部区域进行面部标志检测,并将其检测到的68个点转换为方便python处理的Numpy array
-                            #     gray = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
-                            #     shape = predictor(gray, rect)
-                            #     shape = face_utils.shape_to_np(shape)
+                            rects = detector(im0,1)
+                            for (i, rect) in enumerate(rects):
+                                # 确定面部区域进行面部标志检测,并将其检测到的68个点转换为方便python处理的Numpy array
+                                gray = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
+                                shape = predictor(gray, rect)
+                                shape = face_utils.shape_to_np(shape)
 
                             
-                            #     # 循环遍历面部标志独立的每一部分
-                            #     for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
-                            #         if name == "inner_mouth":
-                            #             # 复制一张原始图的拷贝,以便于绘制面部区域,及其名称
-                            #             clone = im0.copy()
-                            #             # 遍历独立的面部标志的每一部分包含的点,并画在图中
-                            #             for (x, y) in shape[i:j]:
-                            #                 #cv2.circle(im0, (x, y), 1, (0, 0, 255), -1)
-                            #                 # 要实际提取每个面部区域，我们只需要计算与特定区域关联的（x，y）坐标的边界框，并使用NumPy数组切片来提取它：
-                            #                 (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
-                            #                 #cv2.rectangle(im0,(x,y),(x+w,y+h),(0,0,255),thickness=1)
-                            #                 mouth_y,mouth_h,mouth_x,mouth_w = y,h,x,w
+                                # 循环遍历面部标志独立的每一部分
+                                for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+                                    if name == "inner_mouth":
+                                        # 复制一张原始图的拷贝,以便于绘制面部区域,及其名称
+                                        clone = im0.copy()
+                                        # 遍历独立的面部标志的每一部分包含的点,并画在图中
+                                        for (x, y) in shape[i:j]:
+                                            #cv2.circle(im0, (x, y), 1, (0, 0, 255), -1)
+                                            # 要实际提取每个面部区域，我们只需要计算与特定区域关联的（x，y）坐标的边界框，并使用NumPy数组切片来提取它：
+                                            (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
+                                            #cv2.rectangle(im0,(x,y),(x+w,y+h),(0,0,255),thickness=1)
+                                            mouth_y,mouth_h,mouth_x,mouth_w = y,h,x,w
                             
                             ########### 有嘴唇检测
                             #提取烟头方框的坐标数值,为左上角和右下角的坐标
-                            # smoke_left_up,smoke_right_down=(int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
-                            # if xyxy[3] < mouth_y or xyxy[1] > mouth_y+mouth_h or xyxy[0]>mouth_x+mouth_w or xyxy[2]<mouth_x:
-                            #     if conf > 0.84:  #在距离嘴巴比较远的位置时，如果conf>0.8则判断为烟头
-                            #         #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)      
-                            #         detect_flag = detect_flag + 1
-                            #     else:   
-                            #         print('')
-                            #         #print("NO drawing. It is not smoking!.......................................")
-                            # else:
-                            #     if conf >0.75:   #在距离嘴巴比较近的位置时，如果conf>0.7，才判断为烟头   
-                            #         #print("drawing this retrangle................................")
-                            #         detect_flag = detect_flag + 1
-                            #         #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                            smoke_left_up,smoke_right_down=(int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
+                            if xyxy[3] < mouth_y or xyxy[1] > mouth_y+mouth_h or xyxy[0]>mouth_x+mouth_w or xyxy[2]<mouth_x:
+                                if conf > 0.85:  #在距离嘴巴比较远的位置时，如果conf>0.8则判断为烟头
+                                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)      
+                                    detect_flag = detect_flag + 1
+                                else:   
+                                    print('')
+                                    #print("NO drawing. It is not smoking!.......................................")
+                            else:
+                                if conf >0.75:   #在距离嘴巴比较近的位置时，如果conf>0.7，才判断为烟头   
+                                    #print("drawing this retrangle................................")
+                                    detect_flag = detect_flag + 1
+                                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
                             
                             ########### 无嘴唇检测
-                            if conf > 0.61:  #在距离嘴巴比较远的位置时，如果conf>0.8则判断为烟头
-                                # plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)      
-                                detect_flag = detect_flag + 1
+                            # if conf > 0.61:  #在距离嘴巴比较远的位置时，如果conf>0.8则判断为烟头
+                            #     # plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)      
+                            #     detect_flag = detect_flag + 1
 
                             ##########################################
                             ### 结束
                             ##########################################
 
                 # Save results (image with detections)
-                # if save_img:
-                #     if dataset.mode == 'images':
-                #         cv2.imwrite(save_path, im0)
-                #     else:
-                #         if vid_path != save_path:  # new video
-                #             vid_path = save_path
-                #             if isinstance(vid_writer, cv2.VideoWriter):
-                #                 vid_writer.release()  # release previous video writer
+                if save_img:
+                    if dataset.mode == 'images':
+                        cv2.imwrite(save_path, im0)
+                    else:
+                        if vid_path != save_path:  # new video
+                            vid_path = save_path
+                            if isinstance(vid_writer, cv2.VideoWriter):
+                                vid_writer.release()  # release previous video writer
 
-                #             fourcc = 'mp4v'  # output video codec
-                #             fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                #             w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                #             h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                #             vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-                        #vid_writer.write(im0)
+                            fourcc = 'mp4v'  # output video codec
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                            vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+                        vid_writer.write(im0)
 
     # --------------------------- 打印每一个视频的数据    
     if save_txt or save_img:
@@ -212,20 +213,20 @@ def detect(save_img=False,output_file=None,source_file=None):
     print("images_total = ", image_total)
 
     ############ 有嘴唇检测
-    # if detect_flag >= 2 and detect_flag<45:
-    #     print("This video is smoking!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    #     return True,detect_flag,image_total
-    # else:
-    #     print("This video is no smoking.")
-    #     return False,detect_flag,image_total
-    
-    ########### 无嘴唇检测
-    if detect_flag > 0:
+    if detect_flag >= 2 and detect_flag<45:
         print("This video is smoking!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         return True,detect_flag,image_total
     else:
         print("This video is no smoking.")
         return False,detect_flag,image_total
+    
+    ########### 无嘴唇检测
+    # if detect_flag > 0:
+    #     print("This video is smoking!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    #     return True,detect_flag,image_total
+    # else:
+    #     print("This video is no smoking.")
+    #     return False,detect_flag,image_total
 
 
 class Detect:
@@ -300,9 +301,13 @@ class Detect:
 
 if __name__ == '__main__':
 
-    images_path = "inference\images\\50_50_test"        # 存放照片文件的位置
+    # images_path = "inference\images\\50_50_test\\False_image"        # 存放照片文件的位置
+    images_path = "inference\images\\6"        # 存放照片文件的位置
+    
     images_file =  os.listdir(images_path)  # 读取每一个文件夹下面的文件名
+    # print(images_file)
 
+    
     for j in range(len(images_file)):
         parser = argparse.ArgumentParser()
         folder = 1
@@ -310,9 +315,12 @@ if __name__ == '__main__':
         # parser.add_argument('--source', type=str, default='./inference/images/smoke_false_img/{}'.format(folder), help='source')  # file/folder, 0 for webcam
         # parser.add_argument('--output', type=str, default='inference/output/smoke_false_img/{}'.format(folder), help='output folder')  # output folder
 
-        parser.add_argument('--source', type=str, default='./inference/images/50_50_test/{}/'.format(images_file[j]), help='source')  # file/folder, 0 for webcam
-        parser.add_argument('--output', type=str, default='inference/output/50_50_test/original_less_fps/061/{}/'.format(images_file[j]), help='output folder')  # output folder
-        parser.add_argument('--excel_file', type=str, default='./inference/output/50_50_test/original_less_fps/061/{}.xlsx'.format(images_file[j]), help='excel folder')  # output folder
+        # parser.add_argument('--source', type=str, default='./inference/images/50_50_test/False_image/{}/'.format(images_file[j]), help='source')  # file/folder, 0 for webcam
+        # parser.add_argument('--output', type=str, default='inference/output/50_50_test/False_image/{}/'.format(images_file[j]), help='output folder')  # output folder
+        # parser.add_argument('--excel_file', type=str, default='./inference/output/50_50_test/False_image/{}.xlsx'.format(images_file[j]), help='excel folder')  # output folder
+        parser.add_argument('--source', type=str, default='./inference/images/6/{}/'.format(images_file[j]), help='source')  # file/folder, 0 for webcam
+        parser.add_argument('--output', type=str, default='inference/output/6/{}/'.format(images_file[j]), help='output folder')  # output folder
+        parser.add_argument('--excel_file', type=str, default='./inference/output/6/{}.xlsx'.format(images_file[j]), help='excel folder')  # output folder
 
         parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
         parser.add_argument('--conf-thres', type=float, default=0.36, help='object confidence threshold')
@@ -327,12 +335,12 @@ if __name__ == '__main__':
         opt = parser.parse_args()
 
         #create excel files 
-        workbook = xlsxwriter.Workbook(opt.excel_file)
-        worksheet = workbook.add_worksheet()
-        workbook.close()
+        # workbook = xlsxwriter.Workbook(opt.excel_file)
+        # worksheet = workbook.add_worksheet()
+        # workbook.close()
         
         #-----------------------
-        print("Writing excel files {}".format(opt.excel_file))
+        # print("Writing excel files {}".format(opt.excel_file))
 
         with torch.no_grad():
             if opt.update:  # update all models (to fix SourceChangeWarning)
@@ -350,43 +358,44 @@ if __name__ == '__main__':
 
                 all_files = os.listdir(opt.source)
                 # print((all_files))
+
+                
                 total_num = len(all_files)
+                # print(total_num)
+                
                 Smoke_video_num = 0     # 被检测是抽烟视频的数目
                 picture_num = 0         # 被检测的照片数目
                 picture_total = 0       # total amount of pictures in one file
-                data = openpyxl.load_workbook(opt.excel_file)
-                for i in range(total_num):
-                    source_file = opt.source + "{}".format(all_files[i])
-                    output_file = opt.output + "{}".format(all_files[i])
-                    #source_file = './inference/images/smoke_true_img/{}'.format(all_files[i])
-                    #output_file = 'inference/output/smoke_ture_img/{}'.format(all_files[i])
-                    print(source_file,output_file)
+                # data = openpyxl.load_workbook(opt.excel_file)
+                # for i in range(total_num):
+                # source_file = opt.source + "{}".format(all_files[i])
+                # output_file = opt.output + "{}".format(all_files[i])
 
-                    start = time.time()
-                    Smoke_flag,picture_num,picture_total = detect(output_file=output_file,source_file=source_file)
-                    end = time.time()
-                    print("time consume:",end - start)
-                    
-                    ####################
-                    # 写入excel文件
-                    ####################
+                start = time.time()
+                Smoke_flag,picture_num,picture_total = detect(output_file=opt.output,source_file=opt.source)
+                end = time.time()
+                print("time consume:",end - start)
+                
+                ####################
+                # 写入excel文件
+                ####################
 
-                    # 取第一张表
-                    sheetnames = data.get_sheet_names()
-                    table = data.get_sheet_by_name(sheetnames[0])
-                    table = data.active
-                    value = 1
-                    table.cell(i+1,1).value = all_files[i]  # 视频名
-                    table.cell(i+1,2).value = Smoke_flag    # 检测结果
-                    table.cell(i+1,3).value = "False"
-                    table.cell(i+1,4).value = picture_num   # 识别为抽烟的帧率
-                    table.cell(i+1,5).value = picture_total
-                    data.save(opt.excel_file)
-                    # 输出检测结果
-                    if Smoke_flag:
-                        Smoke_video_num = Smoke_video_num + 1
-                        #print("Smoke_video_num = ",Smoke_video_num)
-                print("Smoke_video_num = ",Smoke_video_num)
+                # 取第一张表
+                # sheetnames = data.get_sheet_names()
+                # table = data.get_sheet_by_name(sheetnames[0])
+                # table = data.active
+                # value = 1
+                # table.cell(i+1,1).value = all_files[i]  # 视频名
+                # table.cell(i+1,2).value = Smoke_flag    # 检测结果
+                # table.cell(i+1,3).value = "False"
+                # table.cell(i+1,4).value = picture_num   # 识别为抽烟的帧率
+                # table.cell(i+1,5).value = picture_total
+                # data.save(opt.excel_file)
+                # 输出检测结果
+                if Smoke_flag:
+                    Smoke_video_num = Smoke_video_num + 1
+                    #print("Smoke_video_num = ",Smoke_video_num)
+            print("Smoke_video_num = ",Smoke_video_num)
                 
         # from queue import Queue
         # from threading import Thread
