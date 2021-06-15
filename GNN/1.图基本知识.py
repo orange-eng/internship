424e1c88e5b4db41c737ef3b943a9d609d276428
orange-eng <972353371@qq.com>
2021-06-15 17:28:13 +0800

    GNN

---
+++ GNN/1.图基本知识.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+edges = pd.DataFrame()
+edges['sources'] = [0,1,2,3,4,4,6,7,7,9,1,4,4,4,6,7,5,8,9,8]
+edges['targets'] = [1,4,4,4,6,7,5,8,9,8,0,1,2,3,4,4,6,7,7,9]
+#edges['weights'] = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+G = nx.from_pandas_edgelist(edges, source='sources',target='targets')
+# degree
+print(nx.degree(G))
+# 连通分量
+print(list(nx.connected_components(G)))
+# 图直径
+print(nx.diameter(G))
+# 度中心性
+print('度中心性',nx.degree_centrality(G))
+# 特征向量中心性
+print('特征向量中心性',nx.eigenvector_centrality(G))
+# betweenness
+print('betweenness',nx.betweenness_centrality((G)))
+# closeness
+print('closeness',nx.closeness_centrality(G))
+# pagerank
+print('pagerank',nx.pagerank(G))
+# HITS
+print('HITS',nx.hits(G,tol=0.00001))
--- GNN/test.py
+++ GNN/test.py
@@ -6,13 +6,7 @@ import networkx as nx
 import pandas as pd
 
 df = pd.DataFrame()
-df['source'] = [1,1,1,2,2,3,3,4,4,5,5,5]    #表示起始点
-df['targets'] = [2,4,5,3,1,2,5,1,5,1,3,4]   #表示终止点
-df['weights'] = [1,1,1,1,1,1,1,1,1,1,1,1]   #从起始点到终止点连线的权重
-G = nx.from_pandas_edgelist(df,source='source',target='targets',edge_attr='weights')
-
-#g = nx.from_pandas_edgelist(df,create_using=nx.DiGraph())
-#print(list(g.neighbors(0)))
-print(nx.degree(G))
-print(list(nx.connected_components(G)))
-print(nx.degree_centrality(G))
+df['source'] = [0,1]
+df['target'] = [1,2]
+g = nx.from_pandas_edgelist(df,create_using=nx.DiGraph())
+print(list(g.neighbors(0)))
