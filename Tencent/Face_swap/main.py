9d10c88952faf92e4f104ed1404fffd1bf93762c
orange <>
2021-07-28 09:06:31 +0800

    face swap

---
+++ Tencent/Face_swap/main.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+import os
+import cv2
+import argparse
+
+from face_detection import select_face
+from face_swap import face_swap
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='FaceSwapApp')
+    parser.add_argument('--src', default="imgs/test9.jpg", help='Path for source image')
+    parser.add_argument('--dst', default="imgs/test6.jpg", help='Path for target image')
+    parser.add_argument('--out', default="results/output9_6.jpg", help='Path for storing output images')
+    parser.add_argument('--warp_2d', default=False, action='store_true', help='2d or 3d warp')
+    parser.add_argument('--correct_color', default=True, action='store_true', help='Correct color')
+    parser.add_argument('--no_debug_window', default=False, action='store_true', help='Don\'t show debug window')
+    args = parser.parse_args()
+
+    # Read images
+    src_img = cv2.imread(args.src)
+    dst_img = cv2.imread(args.dst)
+
+    # Select src face
+    src_points, src_shape, src_face = select_face(src_img)
+    # Select dst face
+    dst_points, dst_shape, dst_face = select_face(dst_img)
+
+    if src_points is None or dst_points is None:
+        print('Detect 0 Face !!!')
+        exit(-1)
+
+    output = face_swap(src_face, dst_face, src_points, dst_points, dst_shape, dst_img, args)
+
+    dir_path = os.path.dirname(args.out)
+    if not os.path.isdir(dir_path):
+        os.makedirs(dir_path)
+
+    cv2.imwrite(args.out, output)
+
+    ##For debug
+    if not args.no_debug_window:
+        cv2.imshow("From", dst_img)
+        cv2.imshow("To", output)
+        cv2.waitKey(0)
+        
+        cv2.destroyAllWindows()
